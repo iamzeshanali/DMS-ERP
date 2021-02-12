94d03e920373a25584e8f81635d55fbdfb0ab047
Zeeshan-Ali <zeeshanalibenifshi@gmail.com>
2021-02-12 10:50:34 +0500

    Enum Entities Added

---
+++ app/Domain/Entities/Enums/DiscountTypeEnum.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Domain\Entities\Enums;
+
+
+use Dms\Core\Model\Object\Enum;
+use Dms\Core\Model\Object\InvalidEnumValueException;
+use Dms\Core\Model\Object\PropertyTypeDefiner;
+
+class DiscountTypeEnum extends Enum
+{
+    const PERCENTAGE = 'percentage';
+    const DOLLAR_AMOUNT = 'dollarAmount';
+
+    /**
+     * @return array
+     */
+    public static function getNamedMap()
+    {
+        return [
+            DiscountTypeEnum::PERCENTAGE=> 'percentage',
+            DiscountTypeEnum::DOLLAR_AMOUNT=> 'dollarAmount',
+
+        ];
+    }
+
+    /**
+     * Defines the type of the options contained within the enum.
+     *
+     * @param PropertyTypeDefiner $values
+     *
+     * @return void
+     */
+    protected function defineEnumValues(PropertyTypeDefiner $values)
+    {
+        $values->asString();
+    }
+
+    /**
+     * @return DiscountTypeEnum
+     * @throws InvalidEnumValueException
+     */
+    public static function percentage () : self{
+        return new self(self::PERCENTAGE);
+    }
+
+    /**
+     * @return DiscountTypeEnum
+     * @throws InvalidEnumValueException
+     */
+    public static function dollarAmount () : self{
+        return new self(self::DOLLAR_AMOUNT);
+    }
+}
